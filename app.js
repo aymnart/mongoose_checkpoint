f39c6d3dd2c9b9634ba682fdd21891f460538aad
believer297 <amaraymen5@gmail.com>
2024-10-12 11:53:06 +0100

    added schema

--- app.js
+++ app.js
@@ -1,9 +1,26 @@
 const express=require("express");
 const app=express();
 const port = 3001;
+const person = require('./models/person')
 const connectDB = require('./config/ConnectDB');
+
 app.use(express.json());
+
 connectDB();
+
+
+const NewPerson = new person({
+    name : 'Aymen',
+    email : 'aymen@gmail.com',
+    age:23,
+    favoriteFoods : ['pizza','crépe','croissant'],
+})
+
+
+NewPerson.save()
+.then(result=>console.log(result))
+.catch(error=>console.error(error));
+
 
 
 app.listen(port, (err) => {
--- models/person.js
+++ models/person.js
@@ -1,3 +1,14 @@
 const mongoose = require('mongoose');
-const personSchema = mongoose.Schema;
+const personSchema = new mongoose.Schema(
+    {
+        name:{type:String,required:true},
+        email:{type:String,required:true,unique:true},
+        age: Number,
+        favoriteFoods:[String],
+        joined:{type:Date, unique:true, default:Date.now}
+    }
+);
+
+const person = mongoose.model('person',personSchema);
+modules.export = person;
 
